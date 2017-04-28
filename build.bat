a19334c8655fed5de395597520eb1b1d53d81a3a
Matthew Hartstonge <matt@mykro.co.nz>
2017-04-28 22:40:27 +1200

    :arrow_up: refactor variables

--- build.bat
+++ build.bat
@@ -1,4 +1,5 @@
 @ECHO OFF
+SET ISO_MD5_CHECKSUM=b0e3e198a12a2ae8efe89f5167655879
 SET VM_DESCRIPTION=A simplified Linux distribution built from containers, for containers
 SET VM_VERSION=1.0.0
-packer build -force -var "vm_description=%VM_DESCRIPTION%" -var "vm_version=%VM_VERSION%" "packer_rancheros.json"
+packer build -force -var "vm_description=%VM_DESCRIPTION%" -var "vm_version=%VM_VERSION%" -var "iso_md5_checksum=%ISO_MD5_CHECKSUM%" "packer_rancheros.json"
--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
+iso_md5_checksum='b0e3e198a12a2ae8efe89f5167655879'
 vm_description='A simplified Linux distribution built from containers, for containers'
 vm_version='1.0.0'
 packer build \
+    -var "iso_md5_checksum=${iso_md5_checksum}" \
     -var "vm_description=${vm_description}" \
-    -var "vm_version=${vm_version}"         \
+    -var "vm_version=${vm_version}" \
     "packer_rancheros.json"
